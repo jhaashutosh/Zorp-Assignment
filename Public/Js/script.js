72690cd666d2010f3463014afcd63a1b199c8b3e
ashutosh <jhaashutosh58@gmail.com>
2023-10-31 20:19:12 +0530

    31-10-2023: Worked on responsiveness of the page.

--- Public/Js/script.js
+++ Public/Js/script.js
@@ -1,17 +1,30 @@
 let secondSidebarOpen = false;
 
+function toggleMenuBar() {
+  const sidebar = document.querySelector(".sidebar");
+  if (secondSidebarOpen) {
+    sidebar.style.display = "none";
+    secondSidebarOpen = false;
+  } else {
+    sidebar.style.display = "flex";
+    secondSidebarOpen = true;
+  }
+}
+
 function toggleSidebar() {
   const sidebar = document.querySelector(".sidebar");
   const secondSidebar = document.querySelector(".second-sidebar");
 
   if (secondSidebarOpen) {
     secondSidebar.style.width = "0";
+    secondSidebar.style.height = "0";
     secondSidebar.style.padding = "0px";
     secondSidebar.style.border = "none";
     secondSidebar.style.visibility = "hidden";
     secondSidebarOpen = false;
   } else {
     secondSidebar.style.width = "200px";
+    secondSidebar.style.height = "auto";
     secondSidebar.style.padding = "0 20px";
     secondSidebar.style.borderRight = "1px solid rgba(47, 47, 47, 0.2)";
     secondSidebar.style.visibility = "visible";
